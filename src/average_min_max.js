8371a079f132288a33b5fad6424eb2aaa17d9249
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-10-02 22:36:38 +0200

    average_min_max.js

--- src/average_min_max.js
+++ src/average_min_max.js
@@ -1,3 +1,11 @@
-module.exports = function averageMinMax() {
-  // TODO Implement me.
+function averageMinMax(tab) {
+  let average = ([...tab].reduce((a, b) => { return a+b}))/ tab.length
+  console.log(`Wartość maksymalna tablicy: ${Math.max(...tab)}, wartość minimalna tablicy: ${Math.min(...tab)}, średnia tablicy: ${average}`)
 };
+
+
+
+averageMinMax([-42, 0, 42])   // Min: -42 Max: 42 Average: 0
+averageMinMax([30, 20, 100])  // Min: 20 Max: 100 Average: 50
+averageMinMax([-23, -4, -12]) // Min: -23 Max: -4 Average: -13
+
