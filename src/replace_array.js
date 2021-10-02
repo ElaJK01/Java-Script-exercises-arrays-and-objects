66659328cc04a715713086789ec63d93c6f7710e
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-10-03 00:03:58 +0200

    replace_array.js

--- src/replace_array.js
+++ src/replace_array.js
@@ -1,3 +1,15 @@
-module.exports = function replaceArray() {
-  // TODO Implement me.
+
+function replaceArray(tab) {
+  for (let i=0; i<tab.length; i++){
+    if (tab[i][0] === "h" || tab[i][0] === "H"){
+      let newTabElement = tab[i].toUpperCase()
+      tab[i] =newTabElement
+      
+    } 
+  } return tab
 };
+
+
+console.log(replaceArray(["banana", "suit", "hammer"])) // [ 'banana', 'suit', 'HAMMER' ]
+console.log(replaceArray(["Hello", "there"])) // [ 'HELLO', 'there' ]
+console.log(replaceArray(["Hey!", "hey..."])) // [ 'HEY!', 'HEY...' ]
