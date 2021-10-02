b75d3f74c43da2742821cd3b889082fdb1040ecb
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-10-02 22:06:15 +0200

    display full name

--- src/display_full_name.js
+++ src/display_full_name.js
@@ -1,3 +1,9 @@
-module.exports = function displayFullName() {
-  // TODO Implement me.
+function displayFullName(name) {
+  let array = name.split(" "); 
+    return `My name is ${array[array.length - 1]}, ${name}.` 
 };
+
+
+console.log(displayFullName('James Bond'))
+console.log(displayFullName("Ada Lovelace")) // My name is Lovelace, Ada Lovelace
+console.log(displayFullName("Salvador Felipe Jacinto Dalí")) // My name is Dalí, Salvador Felipe Jacinto Dalí
