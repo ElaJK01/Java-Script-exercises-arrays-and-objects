d3d7c21d58b5da8d1061e629ec37bc3c51c10453
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-10-02 22:12:08 +0200

    concat_array.js

--- src/concat_array.js
+++ src/concat_array.js
@@ -1,3 +1,11 @@
-module.exports = function concatArray() {
-  // TODO Implement me.
+function concatArray(tab) {
+  str = tab.join(" ")
+  str = str + "."
+  return str
+  
 };
+
+
+console.log(concatArray(["Hello", "this", "is", "dog"])) // 'Hello this is dog.'
+console.log(concatArray(["All", "I", "want", "for", "christmas", "is", "you"])) // 'All I want for christmas is you.'
+console.log(concatArray([])) // '.'
