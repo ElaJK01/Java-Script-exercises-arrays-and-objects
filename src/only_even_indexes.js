7efd16d8029707e7a647c8c42c335fe3c9a1841b
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-10-03 22:41:07 +0200

    remove double and only_even.js

--- src/only_even_indexes.js
+++ src/only_even_indexes.js
@@ -1,3 +1,14 @@
-module.exports = function evenIndexes() {
-  // TODO Implement me.
+function evenIndexes(txt) {
+  let tab = txt.split("")
+  let tab2 = tab.filter((value, index, arr)=> {
+    return index % 2 === 0
+      
+  }); return tab2
 };
+
+
+
+
+console.log(evenIndexes("lol")) // [ 'l', 'l' ]
+console.log(evenIndexes("You're weird")) // [ 'Y', 'u', 'r', ' ', 'e', 'r' ]
+console.log(evenIndexes("")) // [ ]
--- src/remove_double.js
+++ src/remove_double.js
@@ -1,3 +1,25 @@
-module.exports = function removeDouble() {
-  // TODO Implement me.
+function removeDouble(arr) {
+  return arr.filter((value, index) => {
+    return arr.indexOf(value) === index
+  })
+   
 };
+
+
+
+let stuff = ["suit", "clock", "butter", "suit"];
+console.log(stuff)
+console.log(removeDouble(stuff))
+console.log(stuff)
+
+// [ 'clock', 'butter', 'suit' ] 
+
+
+let mixed = ["hello", "hello", 23, -3, 23, "hello"]
+console.log(removeDouble(mixed))
+console.log(mixed)
+// [ -3, 23, 'hello' ]  
+
+let someStuff = ["kot", "dog", "kot", "pies"]
+console.log(removeDouble(someStuff))
+console.log(someStuff)
